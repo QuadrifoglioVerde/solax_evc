788067345030d69d496c0b65b09f9be3833e92b2
Quadrifoglio Verde <39336548+QuadrifoglioVerde@users.noreply.github.com>
2026-02-17 13:08:12 +0100

    Add files via upload

---
+++ custom_components/solax_evc/__init__.py
@@ -0,0 +1,8 @@
+"""SolaX EVC Integration"""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass, config):
+    """Set up the SolaX EVC component."""
+    return True
