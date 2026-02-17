37f35de1acab28164ec9d63f93819d806287503f
Quadrifoglio Verde <39336548+QuadrifoglioVerde@users.noreply.github.com>
2026-02-17 13:07:31 +0100

    Delete custom_components/__init__.py

--- custom_components/__init__.py
+++
@@ -1,8 +0,0 @@
-"""SolaX EVC Integration"""
-import logging
-
-_LOGGER = logging.getLogger(__name__)
-
-async def async_setup(hass, config):
-    """Set up the SolaX EVC component."""
-    return True
